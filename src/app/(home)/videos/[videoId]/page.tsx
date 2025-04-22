1c9d8b319f4797514c7bc7dbc724920e6a5005eb
Johnson Wang <yuhang0229@gmail.com>
2025-04-22 21:51:44 +1000

    suggestions

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -15,6 +15,11 @@ const Page = async ({ params }: PageProps) => {
     limit: DEFAULT_LIMIT,
   });
 
+  void trpc.suggestions.getMany.prefetchInfinite({
+    videoId,
+    limit: DEFAULT_LIMIT,
+  });
+
   return (
     <HydrateClient>
       <VideoView videoId={videoId} />
